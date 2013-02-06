09fbd67cfbde1e4adba87c289202bb64b2d8f5a5
Josey Morton <me@joseymorton.com>
2013-02-06 11:16:42 -0600

    adding encrypt method

--- Dropbox/OAuth/Storage/Manual.php
+++ Dropbox/OAuth/Storage/Manual.php
@@ -95,6 +95,10 @@ class Manual extends Session
         return $this->encrypt($this->get('access_token'));
     }
 
+    public function encrypt($object){
+        return $this->encrypt($object);
+    }
+
     public function decrypt($token){
         if(is_object($token)) return $token;
         // Decrypt the token if there is an Encrypter instance
